d180596e100c0fe3bba686e013d0e9138a2c42b3
Pim <Pimvgd@gmail.com>
2018-02-05 23:17:26 +0100

    fix RunnableButton (used in command panel pages)

--- NightgamesMod/nightgames/gui/RunnableButton.java
+++ NightgamesMod/nightgames/gui/RunnableButton.java
@@ -3,17 +3,15 @@ package nightgames.gui;
 public abstract class RunnableButton extends KeyableButton {
     private static final long serialVersionUID = 5435929681634872672L;
     private String text;
-    protected Runnable runnable;
     RunnableButton(String text) {
         super(formatHTMLMultiline(text, ""));
         this.text = text;
-        this.runnable = runnable;
         resetFontSize();
 
         getButton().addActionListener((evt) -> this.run());
     }
 
-    public static RunnableButton genericRunnableButton(String text, Runnable runnable) {
+    public static RunnableButton genericRunnableButton(String text, final Runnable runnable) {
         return new RunnableButton(text) {
             private static final long serialVersionUID = -3002901673898389260L;
 
